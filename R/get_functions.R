a1141288e5f6dfbfe7fb0aacca75b17e0fe84c14
djacobs7 <daniel@sibblingz.com>
2019-04-14 18:26:59 -0700

    added the ability to add documentation

--- R/get_functions.R
+++ R/get_functions.R
@@ -28,7 +28,17 @@ addTargetFunctions = function( ... ){
 
 
   lapply( arguments, function(qq){
+    ll = getFunctionPropertiesFromQuosure(qq )
+    storage_hash_table[[ ll$keyname ]] = ll
+  })
+  storage_hash_table
+}
 
+
+getFunctionPropertiesFromQuosure = function(qq ){
+  ##make sure you pass in a quosure
+  if( rlang::is_quosure(qq)){
+    keyname = NULL
     substituted =  rlang::get_expr(qq)
     if( is.call(substituted  )){ #handle function calls
       function_name = substituted[[1]]
@@ -42,18 +52,45 @@ addTargetFunctions = function( ... ){
     environment = environment( rlang::eval_tidy( qq ))
     environmentName = environmentName( environment )
 
-    # need some specific solution when the argument passed in already contains a ::
-    keyname = paste0( environmentName, '::' , deparse( quo_get_expr ( qq )  ) )
-    storage_hash_table[[ keyname ]] = list(
+    if ( is.null(keyname)){
+      keyname = paste0( environmentName, '::' , deparse( rlang::quo_get_expr ( qq )  ) )
+    }
+
+    result = list(
       ref = ref,
       environment = environment,
       environmentName = environmentName,
-      expr = keyname
+      keyname = keyname
     )
-  })
-  storage_hash_table
+  } else{
+    throw("This function expects a quosure")
+  }
+  result
 }
 
+addDocumentationURL = function( targetFunction, urls ){
+  #addTargetFunction( targetFunction )  #not sure how to do this part!
+
+  qq = rlang::enquo( targetFunction )
+
+  props = getFunctionPropertiesFromQuosure( qq )
+  keyname = props$keyname
+  present_list = storage_hash_table[[keyname]]
+  if ( is.null(present_list)){
+    throw("make sure your target function is already added. FIXME!! add addTargetFunction atop")
+  }
+  present_list$urls = unique( c( present_list$urls, urls ) )
+  storage_hash_table[[keyname]] = present_list
+  #but the plan here is you can type in a url and this will persist it along with your function name.  easy breezy.
+}
+
+getDocumentationURL = function( targetFunction ){
+  qq = rlang::enquo( targetFunction )
+
+  props = getFunctionPropertiesFromQuosure( qq )
+  keyname = props$keyname
+  storage_hash_table[[keyname]]$urls
+}
 showTargetFunctions = function(){
   ls ( storage_hash_table)
 }
--- tests/testthat/test-find_expressions.R
+++ tests/testthat/test-find_expressions.R
@@ -5,20 +5,32 @@ test_that("multiplication works", {
 
 #  expression  =
 
-  createTargetFunctions(lm, dplyr::summarize)
+#  createTargetFunctions(lm, dplyr::summarize)
 
   get_functions({ a = iris;
   model = lm( iris$Sepal.Length ~ iris$Petal.Length )} )
   expect_equal(2 * 2, 4)
 })
 
-test_that("can add a target function"{
+test_that("can_add_documentation", {
+  addTargetFunctions(pryr::standardise_call)
+  addDocumentationURL(pryr::standardise_call, "http://adv-r.had.co.nz/Expressions.html#calls")
+
+})
+
+test_that("can add a target function",{
   addTargetFunctions( lm, dplyr::summarise )
 
 
   addTargetFunctions(lm)
   addTargetFunctions(new.env)
   addTargetFunctions(ls)
+
+  currentTargets = showTargetFunctions()
+
+  expect_true( grep( "stats::lm", currentTargets) != 0 )
+  expect_true( grep( "dplyr::summarise", currentTargets) != 0 )
+  expect_true( grep( "base::new.env", currentTargets) != 0 )
 
   # FIND OUT WHAT THESE DO AND TEST THEM
   #createTargetFunctions( 1 )
