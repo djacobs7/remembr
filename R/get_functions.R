e1251014b4465e874d42185e81c9709ed6a651f7
djacobs7 <daniel@sibblingz.com>
2019-05-05 13:17:12 -0700

    Adding the leitner logic

--- R/get_functions.R
+++ R/get_functions.R
@@ -1,6 +1,6 @@
 #https://www.ninds.nih.gov/News-Events/News-and-Press-Releases/Press-Releases/Want-learn-new-skill-Take-some-short-breaks
 
-get_functions= function( expression ){
+print_ast= function( expression ){
 
   sub = substitute(expression)
   print( sub)
@@ -12,16 +12,231 @@ get_functions= function( expression ){
 }
 
 
+#https://stat.ethz.ch/R-manual/R-devel/library/base/html/taskCallback.html
+
+#'
+#'
+#' @param param s-language expression
+#' @param value result of the expression evaluation
+#' @param status logical indicating success or not
+#' @param visible was the output printed
+#'
+#' @param a data object that is accessible to the callback ( passed in from addTaskCallback)
+#'
+#' @return I guess this indicates if the callback succeeded?
+echoExpressionCallback = function(expr, value, status, visible, data){
+  print( paste0( str( expr ) ) )
+
+  TRUE
+}
+
+
+#'
+#'
+#' @param param s-language expression
+#' @param value result of the expression evaluation
+#' @param status logical indicating success or not
+#' @param visible was the output printed
+#'
+#' @param a data object that is accessible to the callback ( passed in from addTaskCallback)
+#'
+#' @return I guess this indicates if the callback succeeded?
+addCallCountsCallback = function(expr, value, status, visible, data){
+
+  get_functions( expr, needs_substitute = FALSE )
+
+
+  if ( opts$should_persist ){
+    saveRDS(  call_counts_hash_table, call_counts_hash_table_path )
+
+  }
+  TRUE
+}
+
+
+clearCallCounts = function(){
+  rm( list= ls( call_counts_hash_table ), envir = call_counts_hash_table)
+}
+
+
+
+showCallCounts = function(){
+  ls( call_counts_hash_table )
+}
+
+
+loadOrCreateEnv = function(path = NULL){
+  if (!is.null(path) & file.exists(path)){
+    readRDS( path )
+  } else {
+    #http://adv-r.had.co.nz/Environments.html
+    #this is a trick for creating private variables.. otherwise packages are not allowed to modify state.
+    #also this is what we wanted to do anyway, a new.env is the same as a hash table in R
+    new.env( hash = TRUE, parent = emptyenv())
+  }
+}
+
+opts = new.env( hash = TRUE, parent = emptyenv())
+opts[["should_persist"]] = TRUE
+storage_file_directory = "~/.rleitner/"
+
+if ( opts[["should_persist"]]){
+  dir.create(storage_file_directory,showWarnings = FALSE)
+}
+
+call_counts_hash_table_path = file.path( storage_file_directory, "call_counts_hash_table.Rds" )
+call_counts_hash_table = loadOrCreateEnv( call_counts_hash_table_path ) #new.env( hash = TRUE, parent = emptyenv())
+
+storage_hash_table_path =file.path( storage_file_directory,"storage_hash_table_path.Rds" )
+storage_hash_table = loadOrCreateEnv( storage_hash_table_path )
+
+removeTaskCallback("addCallCounts")
+addTaskCallback(  addCallCountsCallback, name = "addCallCounts", data = call_counts_hash_table)
+
+
+
+
+get_functions= function( expression, needs_substitute = TRUE ){
+  #print("analyzing functions")
+
+  #TODO: not sure if this is necessary or helpful or doing the right hting
+  calling_enviroment = parent.frame()
+
+  if ( needs_substitute ){
+    sub = substitute(expression )#, calling_enviroment )
+  } else {
+    sub = expression
+  }
+  #print( sub)
+
+  printName = function( name ) {
+    #print( paste0( "name: ", name) );
+    #print(str(name))
+    name
+  }
+
+  ## internal handler for calls in get_functions
+  handleCall = function( call ){
+    #print("HANDLING CALL")
+    #print( call )
+#    shouldStop = FALSE
+#  shouldStop =  tryCatch({
+    standardised_call = pryr::standardise_call( call )
+#    return(FALSE)
+#  },
+#  finally = function(e){
+#    return(FALSE)
+#  }
+#  ), error =  function(e){
+#    print(e)
+#    print("BANANA")
+#    return( TRUE )
+
+#  })
+#  print("BANANA")
+#  print(shouldStop)
+#    shouldStop = FALSE
+#    if (shouldStop == TRUE){ print("APPLE"); return(call); }
+
+
+    #print( standardised_call )
+    #print(str(standardised_call))
+
+    function_name = standardised_call[[1]]
+    #print(str(function_name))
+    #print(paste0('function name: ', deparse(function_name)))
+
+
+    #https://stackoverflow.com/questions/592322/php-expects-t-paamayim-nekudotayim/592326
+    contains_nekudotayim = grepl("::", deparse(function_name))
+    #print (paste0( "has double colon:  ", (contains_nekudotayim)))
+    #if (function_name == "::"){  #handle specific cases where the called function is `::`.  Like when we call  addTargetFunctions( dplyr::summarise )
+    # print( "WE ARE HERE")
+
+    #  keyname = paste0( standardised_call$pkg, "::", standardised_call$name)
+    #}
+
+    if(contains_nekudotayim){
+      #print("has doube colon")
+      #we already got it, so do nothing
+      keyname = deparse(function_name)
+      #make me wonder if we should handle all of this in the name checker instead
+    } else{
+
+      #print( "NOW WE ARE HERE")
+      qq = rlang::quo(function_name)
+
+
+      #FIXME:
+      # so - we are basically assuming that if we are in a nested series of functions, that no function is ever redefined.
+      # this is not the safest of assumptions, and will often be incorrect
+      # however, the only way I can think to handle this is by actually executing the code.
+      # Presently this is written like a parser or a compile-time thing.
+      # In order to properly evaluate the environments of things, we would actually need to do this at runtime; or somehow hook into every method that could potentially modify a calling environment.  Maybe these is a way?
+      qq = rlang::quo_set_expr(qq, function_name)
+      qq = rlang::quo_set_env(qq, calling_enviroment) #TODO: verify this
+
+      expression_string = deparse( rlang::quo_get_expr(qq) )
+      #print(paste0("expression_string", expression_string))
+      tryCatch({
+        environment = pryr::where( expression_string, env = calling_enviroment )
+
+
+      },
+
+      error = function(e){
+
+        stop(e)
+      })
+
+      #print(qq)
+      #environment = environment( rlang::eval_tidy( qq ))
+      environmentName = environmentName( environment )
+      environmentName = gsub( pattern = "package:", replacement =  "", x =  environmentName)
+      keyname = paste0( environmentName , "::", function_name)
+
+    }
+
+    #print( keyname )
+
+    prev_record = call_counts_hash_table[[keyname]]
+
+    if ( is.null (prev_record )){
+      prev_record = list( first_use =   lubridate::now(tzone = 'UTC'),
+                          most_recent_use = lubridate::now(tzone = 'UTC'),
+                          total_uses = 1,
+                          bucket_id = 1 )
+    } else {
+      prev_record  = list(
+        first_use= prev_record$first_use,
+        most_recent_use = lubridate::now(tzone= "UTC"),
+        total_uses = prev_record$total_uses + 1,
+        bucket_id = prev_record$bucket_id + 1
+        )
+    }
+    #names( prev_record ) = c( 'first_use', 'most_recent_use', 'total_uses')
+
+    call_counts_hash_table[[keyname]] = prev_record
+
+    #print( paste0( "call: ", paste0(call, collapse = " ::: ")) );
+    call
+  }
+  #printCall = function( call ) {}
+  printAtomic = function( atomic ){
+    #print( paste0( "atomic: ", atomic) );
+    atomic
+  }
+  globals::walkAST( expr = sub, atomic = printAtomic, name =  printName, call =handleCall)
+}
+
+
 getObjectFromName = function( name ){
   get( name )
 }
 
 
 
-#http://adv-r.had.co.nz/Environments.html
-#this is a trick for creating private variables.. otherwise packages are not allowed to modify state.
-#also this is what we wanted to do anyway, a new.env is the same as a hash table in R
-storage_hash_table = new.env( hash = TRUE, parent = emptyenv())
+
 
 addTargetFunctions = function( ... ){
   arguments = rlang::enquos(...)
@@ -93,4 +308,58 @@ getDocumentationURL = function( targetFunction ){
 }
 showTargetFunctions = function(){
   ls ( storage_hash_table)
+}
+
+#'
+#'
+#'
+#' @import lubridate
+#' @import dplyr
+#' @import tibble
+reminder = function(){
+
+  convertEnvToDataFrame  = function( call_counts_hash_table ){
+    result = eapply(call_counts_hash_table, function(a){
+      a
+    })
+
+    df =do.call( rbind, result) %>%
+      dplyr::as_tibble(rownames = 'function_name') %>%
+      dplyr::mutate_all(unlist) %>%
+      mutate( first_use = lubridate::as_datetime(first_use ),
+              most_recent_use = lubridate::as_datetime(most_recent_use )
+      )
+    df
+  }
+
+  df = convertEnvToDataFrame( call_counts_hash_table )
+
+  #this becomes a default actually!
+  df$bucket_timer = as.numeric( lubridate::minutes(1) )
+  df = df %>%
+    mutate(
+      review_timer = most_recent_use + bucket_timer,
+      needs_review = ifelse(
+        difftime(review_timer, lubridate::now() ) > 0 ,
+        FALSE,
+        TRUE
+      ) )
+
+
+  df %>%
+    filter(needs_review) %>%
+    top_n( 5, desc(review_timer ))
+
+  #how to do the algorithm?
+
+  #1 ) Compute a bucket for each thing
+  #2 ) Default bucket is immediate.
+
+  #buckets: 10 minutes; 1 hour; 1 day; 1 week; 1 month
+
+  #3 ) compute bucket
+  #4 )
+
+  #call_counts_hash_table
+
 }
--- tests/testthat/test-find_expressions.R
+++ tests/testthat/test-find_expressions.R
@@ -1,20 +1,75 @@
 context("test-find_expressions")
 
-test_that("multiplication works", {
+test_that( "more use cases", {
 
+  clearCallCounts()
+
+  testthat::expect_null(  call_counts_hash_table[['stats::lm']] )
+  get_functions({ a = iris;
+  model = lm( iris$Sepal.Length ~ iris$Petal.Length )
+  model = stats::lm( iris$Sepal.Length ~ iris$Petal.Length )} )
+
+  testthat::expect_equal(  call_counts_hash_table[['stats::lm']]$total_uses, 2 )
+})
+
+test_that("multiplication works", {
+  clearCallCounts()
 
 #  expression  =
 
 #  createTargetFunctions(lm, dplyr::summarize)
 
+  testthat::expect_null(  call_counts_hash_table[['stats::lm']] )
   get_functions({ a = iris;
   model = lm( iris$Sepal.Length ~ iris$Petal.Length )} )
-  expect_equal(2 * 2, 4)
+
+
+  testthat::expect_equal(  call_counts_hash_table[['stats::lm']]$total_uses, 1 )
+  get_functions({ summary( data.frame( a = 2, b = 1))} )
+
+  testthat::expect_equal(  call_counts_hash_table[['stats::lm']]$total_uses, 1 )
+  testthat::expect_equal(  call_counts_hash_table[['base::summary']]$total_uses, 1 )
+
+  get_functions( {lm( iris$Sepal.Length ~ iris$Sepal.Width ) })
+
+  testthat::expect_equal(  call_counts_hash_table[['stats::lm']]$total_uses, 2 )
+
+
+
+  #showCallCounts()
+})
+
+test_that("can create a function within an expression", {
+
+  clearCallCounts()
+  get_functions({
+    runLm = function(){ lm( iris$Sepal.Width ~ iris$Petal.Length ) }
+    runLm()
+  })
+  testthat::expect_equal(  call_counts_hash_table[['stats::lm']]$total_uses, 1 )
+
+})
+
+
+testthat::test_that("we handle namespaces properly", {
+
+  clearCallCounts()
+  get_functions({
+    stats::lm( iris$Sepal.Width ~ iris$Petal.Length )
+  })
+  testthat::expect_equal(  call_counts_hash_table[['stats::lm']]$total_uses, 1 )
+
 })
 
 test_that("can_add_documentation", {
   addTargetFunctions(pryr::standardise_call)
   addDocumentationURL(pryr::standardise_call, "http://adv-r.had.co.nz/Expressions.html#calls")
+
+  addTargetFunctions(parent.frame)
+  addDocumentationURL(parent.frame, "http://adv-r.had.co.nz/Environments.html")
+
+  addTargetFunctions(addTaskCallback)
+  addDocumentationURL(addTaskCallback , "http://developer.r-project.org/TaskHandlers.pdf")
 
 })
 
@@ -25,6 +80,7 @@ test_that("can add a target function",{
   addTargetFunctions(lm)
   addTargetFunctions(new.env)
   addTargetFunctions(ls)
+  addTargetFunctions(parent.frame)
 
   currentTargets = showTargetFunctions()
 
